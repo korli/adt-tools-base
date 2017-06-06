037af9404225159988b40dce16da9333bc8ea2f1
Dmitry Avdeev <dmitry.avdeev@jetbrains.com>
2017-06-06 15:58:56 +0300

    GRADLE_EXPERIMENTAL_PLUGIN_RECOMMENDED_VERSION advanced to 0.9.0
    Former-commit-id: 77c088842deb26a2f36109c99861d6f81e00a5e2

--- common/src/main/java/com/android/SdkConstants.java
+++ common/src/main/java/com/android/SdkConstants.java
@@ -1880,7 +1880,7 @@ public final class SdkConstants {
     public static final String GRADLE_PLUGIN_MINIMUM_VERSION = "1.0.0";
     // Always update GRADLE_PLUGIN_RECOMMENDED_VERSION and GRADLE_EXPERIMENTAL_PLUGIN_RECOMMENDED_VERSION together.
     public static final String GRADLE_PLUGIN_RECOMMENDED_VERSION = "2.3.0";
-    public static final String GRADLE_EXPERIMENTAL_PLUGIN_RECOMMENDED_VERSION = "0.8.0";
+    public static final String GRADLE_EXPERIMENTAL_PLUGIN_RECOMMENDED_VERSION = "0.9.0";
     // Temporary - can be removed once 2.3 is the recommended version.
     public static final String GRADLE_PLUGIN_WH_VERSION = "2.3.0-dev";
     public static final String GRADLE_PLUGIN_LATEST_VERSION = GRADLE_PLUGIN_RECOMMENDED_VERSION;
