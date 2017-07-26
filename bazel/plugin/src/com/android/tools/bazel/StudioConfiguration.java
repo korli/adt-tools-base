a099bb4a79d92d4e1680c55fdaf4340aa6db7a8c
Yaroslav Pankratyev <yaroslav.pankratyev@jetbrains.com>
2017-07-26 15:27:06 +0300

    Remove devkit-tests module (fixes test data navigation in devkit tests)
    Former-commit-id: 939fec3b5fd75324a657e72d5f9c2f2f4b2128d6

--- bazel/plugin/src/com/android/tools/bazel/StudioConfiguration.java
+++ bazel/plugin/src/com/android/tools/bazel/StudioConfiguration.java
@@ -61,7 +61,6 @@ public class StudioConfiguration implements Configuration {
         return rule.getLabel().startsWith("//prebuilts/tools/common/m2/repository/")
             || rule.getLabel().startsWith("//tools/vendor/google3/blaze/")
             || rule.getName().endsWith("devkit")  // Kotlin compilation fails
-            || rule.getName().endsWith("devkit-tests")  // depends on devkit
             || rule.getName().endsWith("maven30-server-impl")  // Java compilation fails
             || rule.getName().endsWith("maven32-server-impl")  // Java compilation fails
             || rule.getName().endsWith("cvs-core")  // Java compilation fails
